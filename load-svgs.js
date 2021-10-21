90905c33e7008a0bb3e2c822882be0dc106fa766
HT <91933756+glen-assets@users.noreply.github.com>
2021-10-21 21:19:27 +0800

    Create load-svgs.js

---
+++ load-svgs.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".cloud1, .cloud2").load("https://glen-assets.github.io/music-page-03-stardust/cloud_large.html");
+    
+    $(".cloud3").load("https://glen-assets.github.io/music-page-03-stardust/cloud_alt.html");
+    
+    $(".bgcloud1, .bgcloud3, .bgcloud4").load("https://glen-assets.github.io/music-page-03-stardust/cloud_bg1.html");
+    
+    $(".bgcloud2").load("https://glen-assets.github.io/music-page-03-stardust/cloud_bg2.html");
+    
+    $(".glen-svg").load("https://glen-assets.github.io/music-page-03-stardust/glencrd.html");
+}); //end ready
